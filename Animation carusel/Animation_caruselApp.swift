f9876adfeeea0a89273447f39ec858ad43876e04
Samborskyi <r.samborskyi@gmail.com>
2021-11-26 11:39:20 +0100

    Initial Commit

---
+++ Animation carusel/Animation_caruselApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_caruselApp.swift
+//  Animation carusel
+//
+//  Created by Roman Samborskyi on 26.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Animation_caruselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation carusel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animation carusel
+//
+//  Created by Roman Samborskyi on 26.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
